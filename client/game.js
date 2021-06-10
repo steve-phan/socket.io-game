131d2cca40df3a88f73ef87f2ef2cac0cfaee56e
vUnAmp <phanhaingoc@gmail.com>
2021-06-10 20:01:58 +0200

    moveTo lineTo arc arcTo

--- client/game.js
+++ client/game.js
@@ -6,11 +6,102 @@ const canvas = document.getElementById('ball');
 const ctx = canvas.getContext('2d');
 
 ctx.beginPath();
-ctx.moveTo(25, 120); //coordinate
-ctx.lineTo(100, 75); // Move to this coordinate
-ctx.lineTo(100, 25); // Move to this coordinate
-ctx.lineTo(280, 120);
-ctx.fill(); // After all fill
+ctx.moveTo(83, 116);
+ctx.lineTo(83, 102);
+ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+ctx.lineTo(111, 116);
+ctx.lineTo(106.333, 111.333);
+ctx.lineTo(101.666, 116);
+ctx.lineTo(97, 111.333);
+ctx.lineTo(92.333, 116);
+ctx.lineTo(87.666, 111.333);
+ctx.lineTo(83, 116);
+ctx.fill();
+
+const x = new Path2D(ctx);
+
+x.moveTo(300, 300);
+
+// ctx.beginPath();
+
+// roundedRect(ctx, 12, 12, 150, 150, 15);
+// roundedRect(ctx, 19, 19, 150, 150, 9);
+// roundedRect(ctx, 53, 53, 49, 33, 10);
+// roundedRect(ctx, 53, 119, 49, 16, 6);
+// roundedRect(ctx, 135, 53, 49, 33, 10);
+// roundedRect(ctx, 135, 119, 25, 49, 10);
+
+// ctx.beginPath();
+// ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+// ctx.lineTo(31, 37);
+// ctx.fill();
+
+// for (var i = 0; i < 8; i++) {
+//   ctx.fillRect(51 + i * 16, 35, 4, 4);
+// }
+
+// for (i = 0; i < 6; i++) {
+//   ctx.fillRect(115, 51 + i * 16, 4, 4);
+// }
+
+// for (i = 0; i < 8; i++) {
+//   ctx.fillRect(51 + i * 16, 99, 4, 4);
+// }
+
+// ctx.fillStyle = 'white';
+// ctx.beginPath();
+// ctx.moveTo(91, 96);
+// ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+// ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+// ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+// ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+// ctx.moveTo(103, 96);
+// ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+// ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+// ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+// ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+// ctx.fill();
+
+// function roundedRect(ctx, x, y, width, height, radius) {
+//   ctx.beginPath();
+//   ctx.moveTo(x, y + radius);
+//   ctx.lineTo(x, y + height - radius);
+//   ctx.arcTo(x, y + height, x + radius, y + height, radius);
+//   ctx.lineTo(x + width - radius, y + height);
+//   ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+//   ctx.lineTo(x + width, y + radius);
+//   ctx.arcTo(x + width, y, x + width - radius, y, radius);
+//   ctx.lineTo(x + radius, y);
+//   ctx.arcTo(x, y, x, y + radius, radius);
+//   ctx.stroke();
+// }
+
+// ctx.arcTo(50, 50, 70, 70, 50);
+// ctx.arc(75, 75, 50, 0, Math.PI * 0.2, false); // Outer circle
+
+// ctx.moveTo(110, 75);
+// ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+// ctx.moveTo(65, 65);
+// ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+// ctx.moveTo(95, 65);
+// ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+// ctx.fill();
+// ctx.stroke();
+
+// ctx.moveTo(20, 20);
+// ctx.lineTo(30, 40);
+// ctx.lineTo(40, 50);
+// ctx.moveTo(60, 70);
+// ctx.lineTo(80, 120);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(25, 120); //coordinate
+// ctx.lineTo(100, 75); // Move to this coordinate
+// ctx.lineTo(100, 25); // Move to this coordinate
+// ctx.lineTo(280, 120);
+// ctx.fill(); // After all fill
 
 // ctx.fillRect(25, 25, 100, 100);
 // ctx.clearRect(45, 45, 60, 60);
